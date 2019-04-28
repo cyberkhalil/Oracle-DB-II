b19e8a239cfa95d0c9483a29f4eed58d90fdcd3f
ahmed_afifi1999 <ahmedafifi1500@gmail.com>
2019-04-28 13:37:51 +0300

    fixed error in packge and  make formate

--- packge.sql
+++ packge.sql
@@ -1,39 +1,36 @@
-create or replace package all_course
-is
-PROCEDURE allstudent (
-    s_allstudent   OUT student%rowtype
-);
+CREATE OR REPLACE PACKAGE all_course IS
+    PROCEDURE allstudent (
+        s_allstudent   OUT student%rowtype
+    );
 /*
 p2
 */
- PROCEDURE allcourse (
-    c_allcourse   OUT course%rowtype
-);
-
-PROCEDURE allcourse_credit (
-    c_course   OUT SYS_REFCURSOR
-);
+    PROCEDURE allcourse (
+        c_allcourse   OUT course%rowtype
+    );
+    PROCEDURE allcourse_credit (
+        c_course   OUT SYS_REFCURSOR
+    );
 /*
 
 */
-PROCEDURE tcourse_eq_ttakes (
-    c_course   OUT SYS_REFCURSOR
-);
+    PROCEDURE tcourse_eq_ttakes (
+        c_course   OUT SYS_REFCURSOR
+    );
+    PROCEDURE student_name_and_id (
+        c_course   OUT SYS_REFCURSOR
+    );
+    PROCEDURE count_and_title_of_course (
+        count_and_title_of_course   OUT SYS_REFCURSOR
+    );
+    PROCEDURE insert_student(
+        id IN student.id%type,
+        name IN student.name%type,
+        dept_name IN student.dept_name%type,
+        tot_cred IN student.TOT_CRED%type
+    );
 
-PROCEDURE Student_name_and_ID (
-    c_course   OUT SYS_REFCURSOR
-);
-PROCEDURE count_and_Title_of_course (
-    count_and_Title_of_course   OUT SYS_REFCURSOR
-);
-ROCEDURE Insert_Student (
-    ID   in Student.ID%type,
-        Name   in Student.Name%type,
-        DEPT_Name   in Student.DEPT_Name%type,
-        TOT_CRED   in Student.TOT_CRED%type
-);
-End;
-
+END;
 
 --
 /*
