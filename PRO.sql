51d24ee208ce45acd9d2654206b043d6e96c95e4
mahmoud khalil <khalil2535@protonmail.com>
2019-04-28 06:48:32 +0300

    format PRO
    Signed-off-by: mahmoud khalil <khalil2535@protonmail.com>

--- PRO.sql
+++ PRO.sql
@@ -1,93 +1,111 @@
-Create or Replace PROCEDURE AllStudent(
-S_AllStudent out Student%Rowtype
-)
-is 
-begin     
+CREATE OR REPLACE PROCEDURE allstudent (
+    s_allstudent OUT student%rowtype
+) IS
+BEGIN     
 --select * into S_AllStudent  from Student where Student.ID='19991'; this is for test
-select * into S_AllStudent  from Student where Student.ID='19991';
-    exception
-    when others then 
-    DBMS_OUTPUT.PUT_LINE(sqlerrm);
-end;
+    SELECT
+        *
+    INTO s_allstudent
+    FROM
+        student
+    WHERE
+        student.id = '19991';
+
+EXCEPTION
+    WHEN OTHERS THEN
+        dbms_output.put_line(sqlerrm);
+END;
 --exec AllStudent();
-Declare 
-S_AllStudent Student%Rowtype;
-Begin
-AllStudent(S_AllStudent);
+
+DECLARE
+    s_allstudent student%rowtype;
+BEGIN
+    allstudent(s_allstudent);
    --select * into S_AllStudent  from Student where Student.ID='00128';
-    DBMS_OUTPUT.PUT_LINE(S_AllStudent.Name); -- it should put id in procedure if you want to use this way
-End;
+    dbms_output.put_line(s_allstudent.name); -- it should put id in procedure if you want to use this way
+END;
 
 /*
 P2
 */
 
-Create or Replace PROCEDURE AllCourse(
-C_AllCourse out COURSE%Rowtype
-)
-is 
-begin     
+CREATE OR REPLACE PROCEDURE allcourse (
+    c_allcourse OUT course%rowtype
+) IS
+BEGIN     
 --select * into S_AllStudent  from Student where Student.ID='19991'; this is for test
 --select * into C_AllCourse from COURSE where COURSE_ID='BIO-101';--COURSE_ID
-select * into C_AllCourse from COURSE ;--COURSE_ID
+    SELECT
+        *
+    INTO c_allcourse
+    FROM
+        course;--COURSE_ID
 
-    exception
-    when others then 
-    DBMS_OUTPUT.PUT_LINE(sqlerrm);
-end;
+EXCEPTION
+    WHEN OTHERS THEN
+        dbms_output.put_line(sqlerrm);
+END;
 ---testAllCourse
-Declare 
-C_AllCourse COURSE%Rowtype;
-Begin
-AllCourse(C_AllCourse);
+
+DECLARE
+    c_allcourse course%rowtype;
+BEGIN
+    allcourse(c_allcourse);
    --select * into S_AllStudent  from Student where Student.ID='00128';
-    DBMS_OUTPUT.PUT_LINE(C_AllCourse.Title);
-End;
+    dbms_output.put_line(c_allcourse.title);
+END;
 
 /*
 
 p3
 */
 
-Create or Replace PROCEDURE Allcourse_credit(
-c_COURSE out SYS_REFCURSOR
-)
-is 
-begin     
-open  c_COURSE for
-select  CREDITS into C_AllCourse from COURSE ;--COURSE_ID
+CREATE OR REPLACE PROCEDURE allcourse_credit (
+    c_course OUT SYS_REFCURSOR
+) IS
+BEGIN
+    OPEN c_course FOR SELECT
+                          credits
+                      INTO c_allcourse
+                      FROM
+                          course;--COURSE_ID
 
-    exception
-    when others then 
-    DBMS_OUTPUT.PUT_LINE(sqlerrm);
-end;
+EXCEPTION
+    WHEN OTHERS THEN
+        dbms_output.put_line(sqlerrm);
+END;
 --Test one
-Declare 
-Cursor c_COURSE is select  CREDITS from COURSE ;
-C_Course_CREDITS COURSE.CREDITS%type;
-Begin
 
-open c_COURSE ;
-loop 
-  fetch c_COURSE into C_Course_CREDITS;
-  exit when c_COURSE%notfound;
-     DBMS_OUTPUT.PUT_LINE('Employee Name: ' || C_Course_CREDITS);
-   
-     DBMS_OUTPUT.PUT_LINE('*************************************');
-  end loop;
-  
-close c_COURSE
-;End;
+DECLARE
+    CURSOR c_course IS
+    SELECT
+        credits
+    FROM
+        course;
+
+    c_course_credits course.credits%TYPE;
+BEGIN
+    OPEN c_course;
+    LOOP
+        FETCH c_course INTO c_course_credits;
+        EXIT WHEN c_course%notfound;
+        dbms_output.put_line('Employee Name: ' || c_course_credits);
+        dbms_output.put_line('*************************************');
+    END LOOP;
+
+    CLOSE c_course;
+END;
 --Test two
 
-Declare 
-TYPE c_COURSE IS REF CURSOR;
-C_Course_CREDITS COURSE.CREDITS%type;
-Begin
-Allcourse_credit(c_COURSE);
-loop 
-fetch c_emp into v_record ;
- DBMS_OUTPUT.PUT_LINE(v_record.last_name);
- exit when c_emp%notfound;
- end loop;
-End;
+DECLARE
+    TYPE c_course IS REF CURSOR;
+    c_course_credits course.credits%TYPE;
+BEGIN
+    allcourse_credit(c_course);
+    LOOP
+        FETCH c_emp INTO v_record;
+        dbms_output.put_line(v_record.last_name);
+        EXIT WHEN c_emp%notfound;
+    END LOOP;
+
+END;
